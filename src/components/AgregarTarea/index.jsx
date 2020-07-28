e57afece75aac640e2667689bd1eab32caa00987
carolina <arlinb@gmail.com>
2020-07-28 10:30:59 -0300

    se finalizo primera version

--- src/components/AgregarTarea/index.jsx
+++ src/components/AgregarTarea/index.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
-import { IoMdAddCircle, IoMdTrash } from "react-icons/io";
-import { MdPlaylistAddCheck } from "react-icons/md";
+
+import Input from "../Input";
+import Lista from "../Lista";
+import Filtros from "../Filtros";
 
 const AgregarTarea = () => {
   const [list, setList] = useState([
     { tarea: "comprar lamparita", estado: "completa" },
     { tarea: "comprar cable", estado: "completa" },
   ]);
-  const [text, setText] = useState("");
+
   const [filter, setFilter] = useState(null);
 
-  const agregar = () => {
-    setList([...list, { tarea: text, estado: "incompleta" }]);
+  const agregar = (tarea, setText) => {
+    setList([...list, { tarea, estado: "incompleta" }]);
+    setText("");
   };
 
   const borrar = (item) => {
@@ -22,90 +25,22 @@ const AgregarTarea = () => {
   };
 
   const tachar = (item) => {
-    if (item.estado == "incompleta") {
-      const indexItem = list.indexOf(item);
-      const clone = [...list];
-      clone.splice(indexItem, 1, { tarea: item.tarea, estado: "completa" });
-      setList(clone);
-    } else {
-      const indexItem = list.indexOf(item);
-      const clone = [...list];
-      clone.splice(indexItem, 1, { tarea: item.tarea, estado: "incompleta" });
-      setList(clone);
-    }
+    const indexItem = list.indexOf(item);
+    const clone = [...list];
+    const estado = item.estado == "incompleta" ? "completa" : "incompleta";
+    clone.splice(indexItem, 1, { ...item, estado });
+    setList(clone);
   };
-
   return (
     <div>
-      <div className="input-container">
-        <input
-          onChange={(event) => setText(event.target.value)}
-          value={text}
-          className="input"
-          placeholder="Ingrese una tarea"
-        ></input>
-        <button onClick={agregar} className="btn">
-          <IoMdAddCircle className="icono" />
-        </button>
-      </div>
-      <ul>
-        {list
-          .filter((item) => {
-            if (filter === null) return true;
-
-            return item.estado === filter;
-          })
-          .map((item, indice) => (
-            <li className="tareas" key={indice}>
-              <input
-                type="radio"
-                className="checkbox"
-                defaultChecked={item.estado == "completa"}
-                onClick={() => {
-                  tachar(item);
-                }}
-              ></input>
-              <p
-                style={
-                  item.estado === "completa"
-                    ? { textDecoration: "line-through" }
-                    : null
-                }
-              >
-                {item.tarea}
-              </p>
-              <button onClick={() => borrar(item)} className="btn-trashcan">
-                <IoMdTrash className="trashcan" />
-              </button>
-            </li>
-          ))}
-      </ul>
-      <div className="filtros">
-        <span>{list.length}</span>
-        <MdPlaylistAddCheck />
-        <button
-          className="btn-filtros btn-filtros--todas "
-          onClick={() => {
-            setFilter(null);
-          }}
-        >
-          Todas
-        </button>
-        <button
-          className="btn-filtros btn-filtros--incompletas"
-          onClick={() => setFilter("incompleta")}
-        >
-          Incompletas
-        </button>
-        <button
-          className="btn-filtros btn-filtros--completas "
-          onClick={() => {
-            setFilter("completa");
-          }}
-        >
-          Completas
-        </button>
-      </div>
+      <Input agregar={agregar}></Input>
+      <Lista
+        tachar={tachar}
+        borrar={borrar}
+        list={list}
+        filter={filter}
+      ></Lista>
+      <Filtros list={list} setFilter={setFilter}></Filtros>
     </div>
   );
 };
